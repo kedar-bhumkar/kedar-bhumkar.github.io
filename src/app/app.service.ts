36675d02aeebd17c427460af7b18eb83d3dd32d1
Kedar Bhumkar <kedar.bhumkar@gmail.com>
2023-05-16 15:05:34 -0500

    U

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -13,8 +13,8 @@ export class OktaAuthService {
 
   CLIENT_ID = '0oa95ju89g8tFzckZ5d7';
   ISSUER = 'https://dev-73315710.okta.com/oauth2/aus95oxbnfpnNVnjI5d7'
-  LOGIN_REDIRECT_URI = 'https://springmongodb.onrender.com/callback';
-  LOGOUT_REDIRECT_URI = 'https://springmongodb.onrender.com/login';
+  LOGIN_REDIRECT_URI = 'https://kedar-bhumkar.github.io/callback';
+  LOGOUT_REDIRECT_URI = 'https://kedar-bhumkar.github.io/login';
 
   oktaAuth = new OktaAuth({
     clientId: this.CLIENT_ID,
