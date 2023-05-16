052a1ac4ddf5df790d5680dcb33f45316305abbf
Kedar Bhumkar <kedar.bhumkar@gmail.com>
2023-05-16 13:00:15 -0500

    u

---
+++ src/app/services/dcc.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from "@angular/core";
+import { Observable, of } from "rxjs";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+
+@Injectable({
+  providedIn: "root",
+})
+export class DCCService {
+  private baseUrl = "https://springmongodb.onrender.com/";
+  httpOptions = {
+    headers: new HttpHeaders({ "Content-Type": "application/json" }),
+  
+  constructor(private http: HttpClient) {}
+
+  getLandingPageData(id:any, cmpType:any): Observable<any> {
+    console.log("Inside getLandingPageData.....");
+    //@todo optimize to avoid repeated ws calls on multiple invocations
+    //https://blog.danieleghidoli.it/2020/10/28/angular-how-to-prevent-duplicated-http-requests/
+
+    return this.http.get<any>(this.baseUrl + "user?userId=" + id + "&cmpType="+cmpType);
+  }
+
+  
+  
+  getAppSelectorData(id:any): Observable<any> {
+    
+    console.log("Inside getAppSelectorData.....");
+    //@todo optimize to avoid repeated ws calls on multiple invocations
+    //https://blog.danieleghidoli.it/2020/10/28/angular-how-to-prevent-duplicated-http-requests/
+
+    return this.http.get<any>(this.baseUrl + "user/" + id);
+  }
+
+  getDocs(docType:any): Observable<any> {
+    
+    console.log("Inside getDocs.....");
+    //@todo optimize to avoid repeated ws calls on multiple invocations
+    //https://blog.danieleghidoli.it/2020/10/28/angular-how-to-prevent-duplicated-http-requests/
+
+    return this.http.get<any>(this.baseUrl + "docs/" + docType);
+  }
+
+}
